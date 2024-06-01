eeb886d4fde05c281dd5a257b4a603ce4b6a4fd7
Pavel Voronin <pavel.a.voronin@gmail.com>
2024-06-01 22:38:20 +0800

    natural levels order

--- scenes/tower.gd
+++ scenes/tower.gd
@@ -23,8 +23,7 @@ func hit(damage: int) -> void:
 func init_designs() -> void:
 	for i in range(DESIGNS_AMOUNT):
 		designs.append(i)
-	designs.shuffle()
-
+	designs.reverse()
 
 func set_root() -> void:
 	var p = get_parent()
@@ -35,6 +34,10 @@ func set_root() -> void:
 func _ready() -> void:
 	set_root()
 	init_designs()
+	place_level()
+	place_level()
+	place_level()
+	place_level()
 	place_level()
 	root.get_node("Money").amount_updated.connect(update_buy_button)
 
